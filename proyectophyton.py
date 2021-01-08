cad900c26a3440a3001d079f319e19dedabeac57
Diego Fdo Jaramillo <diegofdojaramillo@outlook.es>
2021-01-07 22:05:14 -0500

    se crea proyecto phyton y nuevas compras

---
+++ proyectophyton.py
@@ -0,0 +1,12 @@
+def run():
+    hour = int(input("ingrese su hora de entrada:"))
+    if hour< 5:
+        print("usted llego antes de la hora")
+    elif hour==5:
+        print("usted llego a la hora")
+    else:
+        print("usted llego tarde")
+
+
+if __name__="__main__":
+    run()
